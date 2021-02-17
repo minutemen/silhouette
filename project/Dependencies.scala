b19e28d580feaf22b7f90f9a79ef0071ad640296
Scala Steward <me@scala-steward.org>
2021-02-17 09:42:55 +0100

    Update cats-core to 2.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   object Version {
     val specs2 = "4.10.5"
     val circe = "0.13.0"
-    val cats = "2.3.1"
+    val cats = "2.4.2"
     val sttp = "3.0.0-RC11"
     val silencer = "1.7.1"
   }
